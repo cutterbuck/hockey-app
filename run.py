bdd02c6936f7fc19be58d73cbdf99ccdabc553c5
Jake <jakemacnaughton@gmail.com>
2024-11-01 10:56:03 -0400

    added waitress wsgi server

--- run.py
+++ run.py
@@ -3,5 +3,7 @@ from package.app import app
 
 if __name__ == '__main__':
     # app.run_server(debug = True, dev_tools_ui = True, use_reloader = True, port=8060)
-    app.server.run()
+    
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
 
