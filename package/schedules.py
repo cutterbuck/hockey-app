53768fab0276cfd5fc2a0680b4c257b25b3d9e95
Jake <jakemacnaughton@gmail.com>
2024-11-04 14:21:00 -0500

    changed to db update

--- package/schedules.py
+++ package/schedules.py
@@ -13,6 +13,6 @@ def start_running_schedules():
     print("Starting schedules")
     now = datetime.now()
     sched = BackgroundScheduler(daemon=True)
-    trigger = CronTrigger(year="*", month="*", day="*", hour="14", minute="20", second="0")
-    sched.add_job(print_hello_message, trigger=trigger, start_date=now, timezone=pytz.timezone("America/New_York"))
+    trigger = CronTrigger(year="*", month="*", day="*", hour="14", minute="25", second="0")
+    sched.add_job(run_standings_auto_update, trigger=trigger, start_date=now, timezone=pytz.timezone("America/New_York"))
     sched.start()
