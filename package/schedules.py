1ef67b37381f2fe5dd5a57bcc0c7ef564fc92842
Jake <jakemacnaughton@gmail.com>
2024-11-04 14:15:50 -0500

    testing remote timezone

--- package/schedules.py
+++ package/schedules.py
@@ -13,6 +13,6 @@ def start_running_schedules():
     print("Starting schedules")
     now = datetime.now()
     sched = BackgroundScheduler(daemon=True)
-    trigger = CronTrigger(year="*", month="*", day="*", hour="19", minute="20", second="0")
+    trigger = CronTrigger(year="*", month="*", day="*", hour="14", minute="20", second="0")
     sched.add_job(print_hello_message, trigger=trigger, start_date=now, timezone=pytz.timezone("America/New_York"))
     sched.start()
