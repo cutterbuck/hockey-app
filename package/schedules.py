adae35c2f5578fc6573782c8730b61578f56bc34
Jake <jakemacnaughton@gmail.com>
2024-11-04 10:41:26 -0500

    added scheduler

---
+++ package/schedules.py
@@ -0,0 +1,18 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+from datetime import datetime
+import pytz
+from package.etl.etl import get_standings
+
+
+
+def print_hello_world():
+    print('Hello world! Time is', datetime.now())
+
+def start_running_schedules():
+    now = datetime.now()
+    sched = BackgroundScheduler(daemon=True)
+    trigger = CronTrigger(year="*", month="*", day="*", hour="10", minute="35", second="0")
+    # sched.add_job(get_standings, trigger=trigger, start_date=now, timezone=pytz.timezone("America/New_York"))
+    sched.add_job(print_hello_world, 'interval', minutes=1, start_date=now, end_date=datetime(now.date().year, now.date().month, now.date().day, 23, 59), timezone=pytz.timezone("America/New_York"))
+    sched.start()
--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from package.app import app
+from package.schedules import start_running_schedules
 
 
 if __name__ == '__main__':
-    # app.run_server(debug = True, dev_tools_ui = True, use_reloader = True, port=8060)
+    start_running_schedules()
+    # app.run_server(debug = True, dev_tools_ui = True, use_reloader = False, port=8060)
 
     from waitress import serve
     serve(app.server, host="0.0.0.0", port=8080)
