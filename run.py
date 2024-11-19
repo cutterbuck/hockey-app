08ff0ad8dcc38b65fd6e77d0b6f81a6d9b25fd53
Jake <jakemacnaughton@gmail.com>
2024-11-19 18:49:46 -0500

    testing build/deploy

--- run.py
+++ run.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
 
     # app.run_server(debug = True, dev_tools_ui = True, use_reloader = True, port=8060)
 
+
     from waitress import serve
     serve(app.server, host="0.0.0.0", port=8080)
-
